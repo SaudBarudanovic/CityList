41328ff6749dbfc1db86e944f6372c693ff4dc72
Saud Barudanovic <saud.barudanovic@gmail.com>
2019-05-15 21:47:47 +0200

    Created a JSON file with countries and corresponding Alpha-2 country codes

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,31 +4,33 @@ $(document).ready(function() {
     htmlMode: true,
     theme: "dracula"
   });
-  $.ajax({
-    url: "cities.json",
-    type: 'GET',
-    dataType: 'JSON',
-    success: function(res) {
-      var cityArray = [];
-      var cityString = '';
-      for (i in res) {
-        if (res[i].country === "BA"){
-          var currentCity = res[i].name;
-          var sterilisedValue = currentCity.replace(/\s/g, '').toLowerCase();
-          sterilisedValue = sterilisedValue.replace(/ć/g, 'c');
-          sterilisedValue = sterilisedValue.replace(/č/g, 'c');
-          sterilisedValue = sterilisedValue.replace(/ž/g, 'z');
-          sterilisedValue = sterilisedValue.replace(/đ/g, 'd');
-          sterilisedValue = sterilisedValue.replace(/š/g, 's');
-          var value = '  <option value="' + sterilisedValue + '">' + currentCity + '</option>';
-          cityArray.push(value)
+  $("#generate-button").click(function() {
+    $.ajax({
+      url: "cities.json",
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(res) {
+        var cityArray = [];
+        var cityString = '';
+        for (i in res) {
+          if (res[i].country === "BA") {
+            var currentCity = res[i].name;
+            var sterilisedValue = currentCity.replace(/\s/g, '').toLowerCase();
+            sterilisedValue = sterilisedValue.replace(/ć/g, 'c');
+            sterilisedValue = sterilisedValue.replace(/č/g, 'c');
+            sterilisedValue = sterilisedValue.replace(/ž/g, 'z');
+            sterilisedValue = sterilisedValue.replace(/đ/g, 'd');
+            sterilisedValue = sterilisedValue.replace(/š/g, 's');
+            var value = '  <option value="' + sterilisedValue + '">' + currentCity + '</option>';
+            cityArray.push(value)
+          }
         }
+        cityArray.sort();
+        for (var i = 0; i < cityArray.length; i++) {
+          cityString += cityArray[i] + '\r\n';
+        }
+        cityCode.setValue("<select>\r\n" + cityString + "</select>")
       }
-      cityArray.sort();
-      for (var i = 0; i < cityArray.length; i++){
-        cityString += cityArray[i] + '\r\n';
-      }
-      cityCode.setValue("<select>\r\n" + cityString + "</select>")
-    }
+    });
   });
 });
